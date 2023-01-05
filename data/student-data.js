e9b9c658d983685855214ba25311475743690de1
Yong Park <pyongho0306@gmail.com>
2023-01-04 23:06:46 -0800

    Finish lab

---
+++ data/student-data.js
@@ -0,0 +1,28 @@
+const students = [
+  {
+    name: "Yong",
+    averageScore: 94,
+    failing: false,
+    _id: 12345,
+  },
+  {
+    name: "Claude",
+    averageScore: 96,
+    failing: false,
+    _id: 12346,
+  },
+  {
+    name: "Daniel ",
+    averageScore: 58,
+    failing: true,
+    _id: 12347,
+  },
+  {
+    name: "Jiwoo ",
+    averageScore: 65,
+    failing: true,
+    _id: 12348,
+  },
+];
+
+export { students };
---
+++ server.js
@@ -0,0 +1,36 @@
+// import modules
+
+import express from "express";
+import { students } from "./data/student-data.js";
+
+// Create Express app
+
+const app = express();
+
+// Configure the app (app.set)
+
+app.set("view engine", "ejs");
+
+// Mount Middleware (app.use)
+
+// Mount routes
+
+app.get("/", function (req, res) {
+  res.redirect("/home");
+});
+
+app.get("/home", function (req, res) {
+  res.render("home");
+});
+
+app.get("/students", function (req, res) {
+  res.render("students/index", {
+    students,
+  });
+});
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function () {
+  console.log("Listening on port 3000");
+});
